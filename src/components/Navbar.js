e418ddf38b6a4432fbcff12452fae93cc2287cd0
LM Train <layconmm@msn.com>
2019-08-28 19:47:45 -0500

    Create new line of codes for data driving

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,11 @@ function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-primary">
       <span className="navbar-brand mb-0 h1">
+        <a href="/public/index.html">
         <h1 id="restart-game">
           Clicky Game
-        </h1></span>
+        </h1> </a></span>
+        
         <span id="click-message"></span>
         <span id="scores">Score    :    </span>
         <span id="top-scores"></span>       
